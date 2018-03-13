50b6caa3a79111c17de61049fecad6b4fb0b2a01
Kaitlyn Anderson <kaitlyn.anderson@uncorkedstudios.com>
2018-03-13 15:28:35 -0700

    continue work for API request

--- app/src/main/java/android/kaitlynanderson/com/tremor/EarthquakeRequest.kt
+++ app/src/main/java/android/kaitlynanderson/com/tremor/EarthquakeRequest.kt
@@ -1,5 +1,17 @@
 package android.kaitlynanderson.com.tremor
 
+import android.kaitlynanderson.com.tremor.Model.Earthquakes
+import com.google.gson.Gson
+import java.net.URL
+
 /**
  * Created by kaitlynanderson on 3/13/18.
  */
+class EarthquakeRequest() {
+    private val API_URL = "http://api.geonames.org/earthquakesJSON?formatted=true&north=44.1&south=-9.9&east=-22.4&west=55.2&username=mkoppelman";
+
+    fun execute(): Earthquakes{
+        val earthquakesJsonString = URL(API_URL).readText()
+        return Gson().fromJson(earthquakesJsonString, Earthquakes::class.java);
+    }
+}
--- app/src/main/java/android/kaitlynanderson/com/tremor/ListActivity.java
+++ app/src/main/java/android/kaitlynanderson/com/tremor/ListActivity.java
@@ -1,12 +1,23 @@
 package android.kaitlynanderson.com.tremor;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-
-import java.util.Date;
 
 /**
  * Created by kaitlynanderson on 3/13/18.
  */
 
 public class ListActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_list);
+
+
+    }
 }
