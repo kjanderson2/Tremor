4bdf6ce646c1da8726ef622e98b358e4b5c31332
Kaitlyn Anderson <kaitlyn.anderson@uncorkedstudios.com>
2018-03-18 22:42:08 -0700

    start on shared prefs

---
+++ app/src/main/java/android/kaitlynanderson/com/tremor/PrefsHelper.java
@@ -0,0 +1,29 @@
+package android.kaitlynanderson.com.tremor;
+
+import android.content.Context;
+
+/**
+ * Created by kaitlynanderson on 3/18/18.
+ */
+
+public class PrefsHelper {
+
+    public static final String KEY_COORDINATE_NORTH = "key_coordinate_north";
+
+    public static final String KEY_COORDINATE_EAST = "key_coordinate_east";
+
+    public static final String KEY_COORDINATE_SOUTH = "key_coordinate_south";
+
+    public static final String KEY_COORDINATE_WEST = "key_coordinate_west";
+
+    public static final String KEY_DATE = "key_date";
+
+    public static final String KEY_MIN_MAGNITUDE = "key_min_magnitude";
+
+    public static final String KEY_MAX_ROWS = "key_max_rows";
+    
+    public static void editPref(Context context, String prefKey, Object newValue) {
+
+    }
+
+}
